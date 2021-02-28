ecc02b4a9638059be272eae377805a68840285e8
Hadi <haditjuta@gmail.com>
2021-02-28 13:46:24 -0600

    2

--- EmbeddedProject1/EmbeddedProject1.cpp
+++ EmbeddedProject1/EmbeddedProject1.cpp
@@ -6,6 +6,12 @@
 
 #ifdef __cplusplus
 extern "C"
+void SystemClock_Config();
+#endif
+
+
+#ifdef __cplusplus
+extern "C"
 #endif
 void SysTick_Handler(void)
 {
@@ -16,6 +22,8 @@ void SysTick_Handler(void)
 int main(void)
 {
 	HAL_Init();
+	
+	SystemClock_Config();
 	
 	int adding = MyAdd(1, 5);
 
@@ -36,4 +44,70 @@ int main(void)
 		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_12, GPIO_PIN_RESET);
 		HAL_Delay(500);
 	}
+}
+
+
+
+
+
+#ifdef __cplusplus
+extern "C"
+#endif
+void SystemClock_Config()
+{ 
+
+	 
+	RCC_OscInitTypeDef RCC_OscInitStruct;
+	RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+	//Configure the main internal regulator output voltage 
+
+	__HAL_RCC_PWR_CLK_ENABLE();
+
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+	//Initializes the CPU, AHB and APB busses clocks 
+
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = 16;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+	RCC_OscInitStruct.PLL.PLLM = 8;
+	RCC_OscInitStruct.PLL.PLLN = 192;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+	RCC_OscInitStruct.PLL.PLLQ = 8;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+		//_Error_Handler(__FILE__, __LINE__);
+	}
+
+	//Initializes the CPU, AHB and APB busses clocks 
+
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+		| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV4;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV16;
+
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+	{
+		//_Error_Handler(__FILE__, __LINE__);
+	}
+
+	HAL_RCC_MCOConfig(RCC_MCO1, RCC_MCO1SOURCE_HSI, RCC_MCODIV_1);
+
+	//Configure the Systick interrupt time 
+
+	HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq() / 1000);
+
+	//Configure the Systick 
+
+	HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+	//SysTick_IRQn interrupt configuration 
+	HAL_NVIC_SetPriority(SysTick_IRQn, 6, 0);
+
+ 
 }
---
+++ EmbeddedProject1/mainTest.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream> 
+ 
+
+int main()
+{ 
+	int a = 3;
+	//int* aptr = &a;
+
+	std::string sd = "hello world test1";
+	std::cout << "test1 hello world";
+	std::cout << sd; 
+	return 1;
+}
